5098e5af2fc7fc122c289c1f0d88f56a782da539
Dilara Bayar <dilarabayar@marun.edu.tr>
2020-05-19 16:45:55 +0300

    Input dosyasını oluşturan kod yazıldı.

---
+++ venv/src/input.py
@@ -0,0 +1,44 @@
+import random
+from random import choice
+
+def main():
+    nucleoids = ["a", "c", "g", "t"]
+    pattern = "aaaaaggggg"
+    string = ""
+    pos = []
+
+    for loop in range(0,10):
+
+        for j in range(0,4):
+
+            if (j == 0):
+                position = random.randint(0,9)
+                pos.append(position)
+            else:
+                position = choice([i for i in range(0,9) if i not in pos])
+                pos.append(position)
+
+            for k in range(0,len(pattern)):
+                if (k == position):
+                    char = random.choice(nucleoids)
+                    while pattern[position] == char:
+                        char = random.choice(nucleoids)
+                    pattern = pattern[:position]+char+pattern[position+1:]
+                    break
+        pos.clear()
+        position = random.randint(0,480)
+        for j in range(0,490):
+            if(j == position):
+                string = string + pattern
+                j = j + 10
+            char = random.choice(nucleoids)
+            string = string + char
+
+        with open('input.txt', 'a') as file:
+            file.write(string + '\n')
+
+        string = ""
+
+
+if __name__ == "__main__":
+    main()
