fa3b5b722429e9facddb2e26496dc87455f8ff64
Dilara Bayar <dilarabayar@marun.edu.tr>
2020-05-26 10:12:43 +0300

    Commentlerin hepsi eklendi.

--- venv/src/input.py
+++ venv/src/input.py
@@ -7,17 +7,19 @@ def main():
     string = ""
     pos = []
 
-    for loop in range(0,10):
+    for loop in range(0,10): #10 kere dnaya yeni pattern satır satır eklemesi yapılacak.
 
+        #bu kısım patternda random 4 yeri değiştirebilemk için.
         for j in range(0,4):
 
-            if (j == 0):
+            if (j == 0): #en başta random bir yer seçilir.
                 position = random.randint(0,9)
-                pos.append(position)
-            else:
+                pos.append(position) #hangi pozisyon değiştirildi tutulur.
+            else: #hiç bir zaman aynı yeri değiştirmesin diye.
                 position = choice([i for i in range(0,9) if i not in pos])
                 pos.append(position)
 
+            #patternda random seçilen pozisyona gidilip oradaki karakter değiştirilir.
             for k in range(0,len(pattern)):
                 if (k == position):
                     char = random.choice(nucleoids)
@@ -25,21 +27,22 @@ def main():
                         char = random.choice(nucleoids)
                     pattern = pattern[:position]+char+pattern[position+1:]
                     break
-        print("yeni pattern: ", pattern)
-        pos.clear()
-        position = random.randint(0,480)
-        for j in range(0,490):
-            if(j == position):
+
+        pos.clear() #tüm posizyonlar silindi.
+        position = random.randint(0,480) #bu pozizyon dnada random yer seçebilmek için.
+        for j in range(0,490): #490 boyutunda random dna oluşturdu.
+            if(j == position): #dnada patternın ekleneceği random yere glince dnaya eklendi.
                 string = string + pattern
-                j = j + 10
+                j = j + 10 #boyutu kadar 10 atlandı.
             char = random.choice(nucleoids)
             string = string + char
 
+        #texte yazdırıldı.
         with open('input.txt', 'a') as file:
             file.write(string + '\n')
 
-        string = ""
-        pattern = "aaaaaggggg"
+        string = "" #string resetlendi.
+        pattern = "aaaaaggggg" #pattern kendi resetlendi.
 
 
 if __name__ == "__main__":
--- venv/src/main.py
+++ venv/src/main.py
@@ -3,29 +3,30 @@ import random
 from random import choice
 
 def randomizedmotifsearch(dna,motifsize,motiflength):
-    counter = 0
+    counter = 0  #Hangi iterasyonda motifi bulduğunu göstermek için
     motifs = [None] * motifsize
     best_motifs = [None] * motifsize
-    allsubstrings = [None] * motifsize
+    allsubstrings = [None] * motifsize #tüm kmerleri tutmak için
     best_score = 0
-    denominator = 0
-    count = 0
+    denominator = 0 #paydayı tutmak için
+    count = 0 #count arrayi
 
     #her sıradan random 10mer seçiyor. motife atıyor.
     for i in range(0, len(dna)):
         rndkmer = random.choice(findsubstring(dna[i], motiflength))
         motifs[i] = rndkmer
 
+    #ilk olarak best motif random çıkan motife eşitlenir.
     best_motifs = motifs.copy()
 
     while True:
-        count, denominator = construct_count(motifs)
-        profile_array = profile(count, denominator)
+        count, denominator = construct_count(motifs) #count oluştur, paydayı bul
+        profile_array = profile(count, denominator) #profile oluştur
         #dnada dizin dizin en iyi sonucu veren kmer bulunuyor. motif bu kmer ile değiştiriliyor.
-        for i in range(0, len(dna)):
-            substring = findsubstring(dna[i], 10).copy()
-            element = probability_randomized(substring, profile_array)
-            del motifs[i]
+        for i in range(0, len(dna)): #row boyunca ilerler.
+            substring = findsubstring(dna[i], motifsize).copy() #dna rowundaki tüm kmerlerin probilitisi hesaplanır.
+            element = probability_randomized(substring, profile_array) #
+            del motifs[i]  #o sıradaki motif silinir, ve yeni en iyi motif konur.
             motifs.insert(i, element)
 
         #eğer sonuç ilkinden daha iyiyse devam
@@ -52,16 +53,17 @@ def gibbsampler(dna,motifsize,motiflength,iteration):
         rndkmer = random.choice(findsubstring(dna[i],motiflength))
         motifs[i] = rndkmer
 
+    # ilk olarak best motif random çıkan motife eşitlenir.
     best_motifs = motifs.copy()
 
-    for i in range(0, iteration):
+    for i in range(0, iteration): #verilen iterasyon sayısı kadar ilerleyecek.
         row = random.randint(0, motifsize-1)  # random bir motif seçilmesi belirleniyor.
         del motifs[row]   #seçilen rowu siliyorum.
-        count, denominator = construct_count(motifs)
-        profile_array = profile(count, denominator)
-        selected_motif = probability_gibbs(findsubstring(dna[row],motiflength), profile_array)
-        motifs.insert(row, selected_motif)
-        if (score(motifs) < score(best_motifs)):
+        count, denominator = construct_count(motifs) #count array ve payda hesaplanır.
+        profile_array = profile(count, denominator) #profil oluşturulur.
+        selected_motif = probability_gibbs(findsubstring(dna[row],motiflength), profile_array) #yeni motif seçilir.
+        motifs.insert(row, selected_motif) #yeni motif motifs arrayine eklenir.
+        if (score(motifs) < score(best_motifs)): #eğer yeni score eskisinden daha düşükse en iyi scoreu değiştir.
             best_motifs = motifs.copy()
 
     print("Motifs that has best score is: ", score(best_motifs))
@@ -72,8 +74,8 @@ def score(motifs):
     score = 0
     nucleoid = [0] * 4
 
-    for i in range(0,len(motifs[0])):
-        for j in range(0,len(motifs)):
+    for i in range(0,len(motifs[0])): #column boyunca ilerler.
+        for j in range(0,len(motifs)): #row boyunca ilerler.
             if (motifs[j][i] == 'a'):
                 nucleoid[0] += 1
             elif (motifs[j][i] == 'c'):
@@ -84,7 +86,7 @@ def score(motifs):
                 nucleoid[3] += 1
         #score columndaki tüm nucleoid sayıları toplamından maximum olan nükleoidin çıkarılmasıyla bulunur.
         score = score + (sum(nucleoid) - max(nucleoid))
-        nucleoid = [0] * 4
+        nucleoid = [0] * 4 #bir sonraki columna geçerken nucleoid araryi resetlenir.
     return score
 
 
@@ -94,8 +96,8 @@ def construct_count(motifs):
     #bana column length lazım o yüzden len(motifs[0]) alındı.
     count = [[0 for i in range(len(motifs[0]))] for j in range(4)]
 
-    for i in range(0,len(motifs[0])):
-        for j in range(0,len(motifs)):
+    for i in range(0,len(motifs[0])): #column boyunca ilerler.
+        for j in range(0,len(motifs)): #row boyunca ilerler.
             if (motifs[j][i] == 'a'):
                 nucleoid[0] += 1
             elif (motifs[j][i] == 'c'):
@@ -104,9 +106,9 @@ def construct_count(motifs):
                 nucleoid[2] += 1
             elif (motifs[j][i] == 't'):
                 nucleoid[3] += 1
-        denominator = sum(nucleoid)
+        denominator = sum(nucleoid) #tüm column toplanarak payda hesaplanır.
 
-        for k in range(0,len(nucleoid)):
+        for k in range(0,len(nucleoid)): #counta bulunan sayıyı a-c-g-t sırasıyla ekle
             count[k][i] = nucleoid[k]
         nucleoid = [0]*4
 
@@ -120,16 +122,17 @@ def construct_count(motifs):
 def profile(count, denominator):
     profile = [[0 for i in range(len(count[0]))] for j in range(4)]
 
-    for i in range(0, len(count)):
+    for i in range(0, len(count)): #tüm arrayi paydaya böl.
         profile[i] = [x/denominator for x in count[i]]
     return profile
 
 def probability_gibbs(motif, profile):
+    #tüm motiflerin üzerinden tek tek char by char ilerliyor.
     probabilities = []
     init = 1.0
 
-    for i in range(0, len(motif)):
-        for j in range(0, len(motif[i])):
+    for i in range(0, len(motif)): #row boyunca ilerler.
+        for j in range(0, len(motif[i])): #column boyunca ilerler.
             if (motif[i][j] == 'a'):
                 init *= profile[0][j]
             elif (motif[i][j] == 'c'):
@@ -138,18 +141,20 @@ def probability_gibbs(motif, profile):
                 init *= profile[2][j]
             elif (motif[i][j] == 't'):
                 init *= profile[3][j]
-        probabilities.append(init)
-        init = 1.0
+        probabilities.append(init) #ilgili motifin probilitysi arraye eklenir.
+        init = 1.0 #initial değer resetlenir.
 
+    #her motifin seçilme olasılığı farklı ve eğer eşit çıkan olursa ilkini alıyor.
     select = random.choices(motif, probabilities)[0]
     return select
 
 def probability_randomized(dna, profile):
+    #tüm dnaki kmerlerin probabilitysi hesaplanacak.
     probabilities = []
     init = 1.0
 
-    for i in range(0, len(dna)):
-        for j in range(0, len(dna[i])):
+    for i in range(0, len(dna)): #row boyunca ilerler.
+        for j in range(0, len(dna[i])): #column boyunca ilerler.
             if (dna[i][j] == 'a'):
                 init *= profile[0][j]
             elif (dna[i][j] == 'c'):
@@ -158,16 +163,19 @@ def probability_randomized(dna, profile):
                 init *= profile[2][j]
             elif (dna[i][j] == 't'):
                 init *= profile[3][j]
-        probabilities.append(init)
-        init = 1.0
+        probabilities.append(init)  #ilgili motifin probilitysi arraye eklenir.
+        init = 1.0 #initial değer resetlenir.
+
     #maximum probability olanın indexini döndür. bana o kmerin valuesu lazım.
     index = probabilities.index(max(probabilities))
+    #dnada o kmerin kendisi geri döndürülür.
     return dna[index]
 
 
 def findsubstring(string,size):
     substring = []
 
+    #gönderilen stringdeki size boyutundaki tüm substringler bulunur.
     for i in range(0, len(string)):
         pattern = string[i:i + size]
         if len(pattern) != size:
